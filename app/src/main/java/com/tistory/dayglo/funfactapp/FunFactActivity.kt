28c2ab64ecf4231a5ea1cf9a9d501c731060e513
ChanJoo <sksckswn@naver.com>
2018-03-23 23:00:29 +0900

    first commit

---
+++ app/src/main/java/com/tistory/dayglo/funfactapp/FunFactActivity.kt
@@ -0,0 +1,12 @@
+package com.tistory.dayglo.funfactapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FunFactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fun_fact)
+    }
+}
